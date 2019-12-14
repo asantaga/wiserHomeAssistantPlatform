cdd284205268fb510149a933e34fdaeecf0e73c9
Mark Parker <11211952+msp1974@users.noreply.github.com>
2019-12-14 15:36:49 +0000

    Show Preset mode in UI and provide ability to boost heating

--- custom_components/wiser/climate.py
+++ custom_components/wiser/climate.py
@@ -14,7 +14,7 @@ from homeassistant.components.climate import ClimateDevice
 #                                                    SUPPORT_TARGET_TEMPERATURE,
 #                                                    HVAC_MODE_HEAT_COOL)
 #
-from homeassistant.components.climate.const import (HVAC_MODE_AUTO, SUPPORT_TARGET_TEMPERATURE, SUPPORT_PRESET_MODE,HVAC_MODE_HEAT_COOL)
+from homeassistant.components.climate.const import (HVAC_MODE_AUTO, SUPPORT_TARGET_TEMPERATURE, SUPPORT_PRESET_MODE, HVAC_MODE_HEAT_COOL)
 from homeassistant.const import (ATTR_BATTERY_LEVEL, ATTR_TEMPERATURE,
                                  TEMP_CELSIUS)
 from homeassistant.helpers.entity import Entity
@@ -22,10 +22,12 @@ from homeassistant.helpers.entity import Entity
 _LOGGER = logging.getLogger(__name__)
 DOMAIN = 'wiser'
 
-PRESET_MANUAL = 'manual'
-PRESET_BOOST = 'boost'
+PRESET_MANUAL = 'Manual'
+PRESET_BOOST = 'Boost'
+PRESET_OVERRIDE = 'Override'
+PRESET_AUTO = 'Schedule'
 
-SUPPORT_FLAGS = SUPPORT_TARGET_TEMPERATURE
+SUPPORT_FLAGS = SUPPORT_TARGET_TEMPERATURE | SUPPORT_PRESET_MODE
 
 def setup_platform(hass, config, add_devices, discovery_info=None):
     """Setup the sensor platform."""
@@ -51,6 +53,7 @@ class WiserRoom(ClimateDevice):
         self.handler = handler
         self.roomId = room_id
         self._hvac_modes_list = [HVAC_MODE_AUTO, HVAC_MODE_HEAT_COOL]
+        self._preset_modes_list = [PRESET_AUTO, PRESET_BOOST]
 
     @property
     def supported_features(self):
@@ -103,7 +106,7 @@ class WiserRoom(ClimateDevice):
         if (state.lower() == "manual"):
             state = HVAC_MODE_HEAT_COOL
         if (state.lower()== "auto"):
-            state=HVAC_MODE_AUTO
+            state = HVAC_MODE_AUTO
         return state
 
     def set_hvac_mode(self, hvac_mode):
@@ -123,6 +126,39 @@ class WiserRoom(ClimateDevice):
         return self._hvac_modes_list
 
     @property
+    def preset_mode(self):
+        preset = self.handler.get_hub_data().getRoom(self.roomId).get('SetpointOrigin')
+        if (preset.lower() == "fromboost"):
+            preset = PRESET_BOOST
+        else:
+            if (preset.lower() == "frommanualoverride"):
+                preset = PRESET_OVERRIDE
+            else:
+                if (preset.lower() == "frommanualmode"):
+                    preset = PRESET_MANUAL
+                else:
+                    preset = PRESET_AUTO
+        return preset
+
+    def set_preset_mode(self, preset_mode):
+        """Set new preset mode."""
+        _LOGGER.debug("*******Setting Preset Mode {} for roomId {}".
+                      format(preset_mode, self.roomId))
+        #Convert HA preset to required api presets
+        if (preset_mode.lower() == 'schedule'):
+            preset_mode = 'auto'
+        if (preset_mode.lower() == 'override' or preset_mode.lower() == 'manual'):
+            preset_mode = 'manual'
+        self.handler.set_room_mode(self.roomId, preset_mode)
+
+        return True
+        
+    @property
+    def preset_modes(self):
+        """Return the list of available preset modes."""
+        return self._preset_modes_list
+      
+    @property
     def target_temperature(self):
         return self.handler.get_hub_data().getRoom(self.roomId). \
                    get('CurrentSetPoint') / 10
@@ -141,6 +177,10 @@ class WiserRoom(ClimateDevice):
         attrs = super().state_attributes
         attrs['percentage_demand'] = self.handler.get_hub_data(). \
             getRoom(self.roomId).get('PercentageDemand')
+        attrs['control_output_state'] = self.handler.get_hub_data(). \
+            getRoom(self.roomId).get('ControlOutputState')
+        attrs['set_point_origin'] = self.handler.get_hub_data(). \
+            getRoom(self.roomId).get('SetpointOrigin')
         attrs['heating_rate'] = self.handler.get_hub_data(). \
             getRoom(self.roomId).get('HeatingRate')
         attrs['window_state'] = self.handler.get_hub_data(). \
