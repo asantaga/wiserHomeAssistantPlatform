fa4e64a53eb10b8798b908b52671e0acc2201b8c
Mark Parker <msparker@sky.com>
2021-12-23 00:58:30 +0000

    v3.0.14

--- custom_components/wiser/const.py
+++ custom_components/wiser/const.py
@@ -7,7 +7,7 @@ Angelosantagata@gmail.com
 """
 DOMAIN = "wiser"
 DATA_WISER_CONFIG = "wiser_config"
-VERSION = "3.0.13"
+VERSION = "3.0.14"
 WISER_PLATFORMS = ["climate", "sensor", "switch", "select", "button"]
 DATA = "data"
 UPDATE_TRACK = "update_track"
