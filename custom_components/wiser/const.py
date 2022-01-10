d06204f7e5a56b48d00c4fc28ef3c2a54f23e39f
Mark Parker <msparker@sky.com>
2022-01-10 16:05:01 +0000

    bump to 3.0.19

--- custom_components/wiser/const.py
+++ custom_components/wiser/const.py
@@ -7,7 +7,7 @@ Angelosantagata@gmail.com
 """
 DOMAIN = "wiser"
 DATA_WISER_CONFIG = "wiser_config"
-VERSION = "3.0.18"
+VERSION = "3.0.19"
 WISER_PLATFORMS = ["climate", "sensor", "switch", "select", "button", "number"]
 DATA = "data"
 UPDATE_TRACK = "update_track"
