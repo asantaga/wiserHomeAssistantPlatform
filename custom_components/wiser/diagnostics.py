de9d647b80a2e251645f3f4c9e5226a91976d861
Mark Parker <msparker@sky.com>
2022-07-11 16:27:39 +0100

    add diagnostics via UI

---
+++ custom_components/wiser/diagnostics.py
@@ -0,0 +1,29 @@
+"""Diagnostics support for Wiser"""
+from __future__ import annotations
+
+from typing import Any
+
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant, callback
+from homeassistant.helpers.device_registry import DeviceEntry
+from wiserHeatAPIv2.cli import anonymise_data
+
+from .const import (
+    DOMAIN
+)
+
+async def async_get_config_entry_diagnostics(
+    hass: HomeAssistant, entry: ConfigEntry
+) -> dict[str, Any]:
+    """Return diagnostics for a config entry."""
+    return _async_get_diagnostics(hass, entry)
+
+@callback
+def _async_get_diagnostics(
+    hass: HomeAssistant,
+    entry: ConfigEntry,
+    device: DeviceEntry | None = None,
+) -> dict[str, Any]:
+    data = hass.data[DOMAIN][entry.entry_id]["data"]
+
+    return anonymise_data(data.wiserhub.raw_hub_data)
