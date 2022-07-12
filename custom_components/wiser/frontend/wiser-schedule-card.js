0d03cd619acc4817cfcc253e470906dfdf604547
Mark Parker <msparker@sky.com>
2022-07-12 17:43:38 +0100

    update schedule card

--- custom_components/wiser/frontend/wiser-schedule-card.js
+++ custom_components/wiser/frontend/wiser-schedule-card.js
@@ -555,9 +555,9 @@ const Et={},St=1,Ct=3,It=4,At=e=>(...t)=>({_$litDirective$:e,values:t});
                     <div class="wrapper" style="white-space: normal; padding-top: 10px;">
                         <div class="day  ${this._show_short_days?"short":""}">&nbsp;</div>
                         <div class="sub-section">
-                            <div class="section-header">Temp</div>
+                            <div class="section-header">${this._show_short_days?"Temp":"Temperature"}</div>
                             <br />
-                            <div style="display: flex; line-height: 32px; width: 100%">
+                            <div style="display: flex; line-height: 32px; width: 100%; max-width: 400px;">
                                 <ha-icon-button
                                     class="set-off-button"
                                     .path=${"M3.28,2L2,3.27L4.77,6.04L5.64,7.39L4.22,9.6L5.95,10.5L7.23,8.5L10.73,12H4A2,2 0 0,0 2,14V22H4V20H18.73L20,21.27V22H22V20.73L22,20.72V20.72L3.28,2M7,17A1,1 0 0,1 6,18A1,1 0 0,1 5,17V15A1,1 0 0,1 6,14A1,1 0 0,1 7,15V17M11,17A1,1 0 0,1 10,18A1,1 0 0,1 9,17V15A1,1 0 0,1 10,14A1,1 0 0,1 11,15V17M15,17A1,1 0 0,1 14,18A1,1 0 0,1 13,17V15C13,14.79 13.08,14.61 13.18,14.45L15,16.27V17M16.25,9.5L17.67,7.3L16.25,5.1L18.25,2L20,2.89L18.56,5.1L20,7.3V7.31L18,10.4L16.25,9.5M22,14V18.18L19,15.18V15A1,1 0 0,0 18,14C17.95,14 17.9,14 17.85,14.03L15.82,12H20C21.11,12 22,12.9 22,14M11.64,7.3L10.22,5.1L12.22,2L13.95,2.89L12.53,5.1L13.95,7.3L13.94,7.31L12.84,9L11.44,7.62L11.64,7.3M7.5,3.69L6.1,2.28L6.22,2.09L7.95,3L7.5,3.69Z"}
@@ -639,7 +639,7 @@ const Et={},St=1,Ct=3,It=4,At=e=>(...t)=>({_$litDirective$:e,values:t});
                 @click=${this._copyDay}
                 ?disabled=${this._activeDay==e||!this._activeDay}
             >
-                ${We.includes(e)?qe[We.indexOf(e)]:e}
+                ${We.includes(e)&&this._show_short_days?qe[We.indexOf(e)]:e}
             </mwc-button>
         `}renderTooltip(e,t){const i=e.slots,o=Ke.includes(i[t].SpecialTime);return N`
           <div class="tooltip-container center">
