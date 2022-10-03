ae1f1d184d927b5cad19788e30c54135e438b4d4
Mark Parker <msparker@sky.com>
2022-10-03 17:44:27 +0100

    v3.1.7

--- custom_components/wiser/const.py
+++ custom_components/wiser/const.py
@@ -10,7 +10,7 @@ DATA_WISER_CONFIG = "wiser_config"
 URL_BASE = "/wiser"
 WISER_CARD_FILENAMES = ["wiser-schedule-card.js", "wiser-zigbee-card.js"]
 
-VERSION = "3.1.7beta1"
+VERSION = "3.1.7"
 WISER_PLATFORMS = ["climate", "sensor", "switch", "select", "button", "number", "light", "cover"]
 DATA = "data"
 UPDATE_TRACK = "update_track"
