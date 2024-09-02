34e3fe9dc635f474d7cfc65c507095bd83430453
LGO44 <95585425+LGO44@users.noreply.github.com>
2024-09-02 18:57:18 +0200

    Version 3.5.1 with wiser hub name

--- custom_components/wiser/const.py
+++ custom_components/wiser/const.py
@@ -5,7 +5,7 @@ https://github.com/asantaga/wiserHomeAssistantPlatform
 Angelosantagata@gmail.com
 
 """
-VERSION = "3.5.0"
+VERSION = "3.5.1"
 DOMAIN = "wiser"
 DATA_WISER_CONFIG = "wiser_config"
 URL_BASE = "/wiser"
