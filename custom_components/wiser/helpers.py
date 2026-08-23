aea7a7f344a69fa6ca5e83f121fc9e38e89af1b9
LGO44 <95585425+LGO44@users.noreply.github.com>
2026-08-23 11:22:29 +0200

    modify name of device to be consistent with the hole entities
    {ENTITY_PREFIX} {device.product_type} {device.name}

--- custom_components/wiser/helpers.py
+++ custom_components/wiser/helpers.py
@@ -80,7 +80,8 @@ def get_device_name(data, device_id, device_type="device"):
             device_room = data.wiserhub.rooms.get_by_id(device.room_id)
             if device_room:
                 return f"{ENTITY_PREFIX} {device_room.name} {device.name}"
-            return f"{ENTITY_PREFIX} {device.name} {device.id}"
+            #return f"{ENTITY_PREFIX} {device.name} {device.id}"
+            return f"{ENTITY_PREFIX} {device.product_type} {device.name} "
 
         if device.product_type == "BoilerInterface":
             return f"{ENTITY_PREFIX} {device.product_type} {device.name}"
