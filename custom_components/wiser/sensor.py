51bb8b17473f4e6bd44e4ef7bab1c74e198e9978
Andrew Blackburn <skyway.float.0r@icloud.com>
2026-09-06 22:39:50 +0100

    fix: use configured frost protection for target sensor

--- custom_components/wiser/sensor.py
+++ custom_components/wiser/sensor.py
@@ -33,7 +33,7 @@ from homeassistant.const import (
 from homeassistant.core import HomeAssistant, callback
 from homeassistant.helpers.update_coordinator import CoordinatorEntity
 
-from aioWiserHeatAPI.wiserhub import TEMP_MINIMUM, TEMP_OFF
+from aioWiserHeatAPI.wiserhub import TEMP_OFF
 
 from .const import (
     DATA,
@@ -1117,7 +1117,7 @@ class WiserLTSTempSensor(WiserSensor):
         else:
             self._state = room_target_temperature(
                 self._data.wiserhub.rooms.get_by_id(self._device_id),
-                TEMP_MINIMUM,
+                self._data.wiserhub.system.away_mode_target_temperature,
                 TEMP_OFF,
             )
         self.async_write_ha_state()
